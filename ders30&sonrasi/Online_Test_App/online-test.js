b16092e3943d04cdc4bcc80ffafe529f7e467bd0
ataberkcetinkaya <chpataberk@outlook.com.tr>
2022-02-19 14:33:46 +0300

    online_test_app (lesson2-JS codes)

---
+++ ders30&sonrasi/Online_Test_App/online-test.js
@@ -0,0 +1,45 @@
+let answersheet = ["D", "D", "D", "D"]; //cevap anahtari, cevaplari bununla kiyaslayacagiz.
+//a bug found; need to give 5 if there are 4 answers bc of the "0" point if condition.
+
+let form = document.querySelector("form");
+let info = document.querySelector(".info");
+
+form.addEventListener("submit", e => {
+    e.preventDefault()
+    
+    let point = 0;
+    let user_answers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value] //kullanicidan alacagimiz cevaplar, birden fazla olacagiz icin array olacak
+                                                                //sorularin degerlerini '.value' ile aliyoruz.
+
+    user_answers.forEach((x, index) => { //index, answersheet'in index'inden geliyor
+        if (x === answersheet[index]) {
+            point = point + 25;
+        }
+    })
+
+    //info.textContent = ("Total Points: " + point);  
+
+    if (point === 0) {
+        info.style.backgroundColor = "red";
+        info.style.color = "white";
+        info.textContent = "Failed with " + point + " points";
+    }
+    if (point === 25) {
+        info.style.backgroundColor = "yellow";
+        info.textContent = "OK. " + point + " points";
+    }
+    if (point === 50) {
+        info.style.backgroundColor = "orange";
+        info.textContent = "Half there. " + point + " points";
+    }
+    if (point === 75) {
+        info.style.backgroundColor = "purple";
+        info.style.color = "white";
+        info.textContent = "Almost there. " + point + " points";
+    }
+    if (point === 100) {
+        info.style.backgroundColor = "green";
+        info.style.color = "white";
+        info.textContent = "Congrats. " + point + " points";
+    }
+})
