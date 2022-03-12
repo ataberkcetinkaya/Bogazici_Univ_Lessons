495d3857f2880d86b6065ec12191279f94866258
ataberkcetinkaya <chpataberk@outlook.com.tr>
2022-03-12 19:35:00 +0300

    jquery_events

--- jquery/jquery_giris/jquery.js
+++ jquery/jquery_giris/jquery.js
@@ -14,7 +14,7 @@
     })
 }) */
 
-$(document).ready(function() {
+/* $(document).ready(function() {
     $("button").click(function() {
         $(this).remove(); //'this' ile burada button kendini yokeder.
     })
@@ -24,4 +24,42 @@ $(document).ready(function() {
     $("button").click(function() {
         $("*").remove(); //'*' ile her şey yokolur.
     })
+}) */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////// Jquery Events //////////////////
+/* $(document).ready(function() {
+    $("button").dblclick(function() { //double-click ile yokolur.
+        $("*").remove();
+    })
+}) */
+
+/* $(document).ready(function() {
+    $("h2").mouseenter(function() { //mouse üzerine gelince etkilesim
+        alert("a yes");
+    })
+})
+
+$(document).ready(function() {
+    $("h2").mouseleave(function() { //mouse ile üzerinden secili alanin disina cikinca etkilesim
+        alert("a no");
+    })
+}) */
+
+/* $(document).ready(function() {
+    $("h2").hover(function() {
+        alert("ur top of it");
+    }, function() {
+        alert("ur not anymore");
+    })
+}) */
+
+$(document).ready(function() {
+    $("input").focus(function() { //focus; input alanina tıklandıgında, etkileşime geçildiginde.
+        $(this).css("background-color", "green"); //this ile bu input'u seçiyoruz ve .css ile css'ine müdahale edecegimizi belirtiyoruz.
+    });
+    $("input").blur(function() { //blur, input alanindan ciktigimizda calisacak
+        $(this).css("background-color", "red");
+        //CSS OZELLIKLERINI HER SEKILDE BURAYA EKLEYEBILIRIZ. FARKLI CSS'LER.
+    });
 })
