6e971056197d6743ac54a9cd3cdb5bc15d2c9e29
ataberkcetinkaya <chpataberk@outlook.com.tr>
2022-03-12 17:59:13 +0300

    jquery_seciciler(class,id,...)

--- jquery/jquery_giris/jquery.js
+++ jquery/jquery_giris/jquery.js
@@ -1,7 +1,27 @@
 //let p_sec = document.querySelector("p"); YERINE JQUERY ILE: $("p") ile seceriz ve nokta atip yapacagimiz islemi yazariz. $("p").click(function) gibi.
         
-$(document).ready(function() { //bu satir CDN'de sorun yasayabiliriz diye
+/* $(document).ready(function() { //bu satir CDN'de sorun yasayabiliriz diye
     $("button").click(function() { //butona tiklandiginda,
         $("p").remove(); //p'yi yoket.
+    })
+}) */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////// Jquery Seçiciler - class/id //////////////////
+/* $(document).ready(function() {
+    $(".tikla").click(function() {
+        $("#atbrk").remove();
+    })
+}) */
+
+$(document).ready(function() {
+    $("button").click(function() {
+        $(this).remove(); //'this' ile burada button kendini yokeder.
+    })
+})
+
+$(document).ready(function() {
+    $("button").click(function() {
+        $("*").remove(); //'*' ile her şey yokolur.
     })
 })
